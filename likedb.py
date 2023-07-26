24301ffe0d2f75ad042a60f6aba622da30a2d441
Mardon1207 <mardonsultonov8@gmail.com>
2023-07-26 15:03:26 +0500

    python data base

--- likedb.py
+++ likedb.py
@@ -1,50 +1,52 @@
 from tinydb import TinyDB,Query
 from tinydb.database import Document
+import json
 class LikeDB:
     def __init__(self,file) -> None:
         self.db=TinyDB(file,indent=4)
-    def save(self,chat_id):
+    def save(self,message_id,chat_id):
+        id=self.db.table(message_id)
         user=Document(
             {
                 "like":0,
                 "dislike":0
             },doc_id=chat_id
         )
-        self.db.insert(user)
-    def like(self,chat_id):
-        like=self.db.get(doc_id=chat_id)["like"]
-        dislike=self.db.get(doc_id=chat_id)["dislike"]
+        id.insert(user)
+    def like(self,message_id,chat_id):
+        like=self.db.table(str(message_id-1)).get(doc_id=chat_id)["like"]
+        dislike=self.db.table(str(message_id-1)).get(doc_id=chat_id)["dislike"]
         if like==0 and dislike==0:
             user={"like":1}
-            self.db.update(user,doc_ids=[chat_id])
+            self.db.table(str(message_id-1)).update(user,doc_ids=[chat_id])
         elif like ==0 and dislike==1:
             user={"like":1,
                   "dislike":0}
-            self.db.update(user,doc_ids=[chat_id])
+            self.db.table(str(message_id-1)).update(user,doc_ids=[chat_id])
         elif like ==1 and dislike ==0:
             user={
                 "like":0
             }
-            self.db.update(user,doc_ids=[chat_id])
-    def dislike(self,chat_id):
-        like=self.db.get(doc_id=chat_id)["like"]
-        dislike=self.db.get(doc_id=chat_id)["dislike"]
+            self.db.table(str(message_id-1)).update(user,doc_ids=[chat_id])
+    def dislike(self,message_id,chat_id):
+        like=self.db.table(str(message_id-1)).get(doc_id=chat_id)["like"]
+        dislike=self.db.table(str(message_id-1)).get(doc_id=chat_id)["dislike"]
         if like==0 and dislike==0:
             user={"dislike":1}
-            self.db.update(user,doc_ids=[chat_id])
+            self.db.table(str(message_id-1)).update(user,doc_ids=[chat_id])
         elif like ==0 and dislike==1:
             user={
                   "dislike":0}
-            self.db.update(user,doc_ids=[chat_id])
+            self.db.table(str(message_id-1)).update(user,doc_ids=[chat_id])
         elif like ==1 and dislike ==0:
             user={
                 "like":0,
                 "dislike":1
             }
-            self.db.update(user,doc_ids=[chat_id])
-    def add_like(self,chat_id):
-        like=self.db.get(doc_id=chat_id)["like"]
+            self.db.table(str(message_id-1)).update(user,doc_ids=[chat_id])
+    def add_like(self,message_id,chat_id):
+        like=self.db.table(str(message_id-1)).get(doc_id=chat_id)["like"]
         return like
-    def add_dislike(self,chat_id):
-        dislike=self.db.get(doc_id=chat_id)["dislike"]
+    def add_dislike(self,message_id,chat_id):
+        dislike=self.db.table(str(message_id-1)).get(doc_id=chat_id)["dislike"]
         return dislike
